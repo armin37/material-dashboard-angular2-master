35311a1da55c02f20b332a1786a40d553ff48281
armin momeni <rmin.momeni@gmail.com>
2019-08-18 09:54:54 +0430

    dashboard and pre intek validation

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+
 export interface PeriodicElement {
     Account: number;
     Name: string;
@@ -9,69 +10,93 @@ export interface PeriodicElement {
 }
 
 
-const ELEMENT_DATA: PeriodicElement[] =[
+const ELEMENT_DATA: PeriodicElement[] = [
     {
-        Account: 1,
-        Name: "Franco Steele",
-        Email: "francosteele@sealoud.com",
-        Phone: "+1 (991) 500-3168",
-        Address: "681 Bartlett Street, Macdona, Indiana, 606"
+        Account: 1251,
+        Name: 'Franco Steele',
+        Email: 'francosteele@sealoud.com',
+        Phone: '+1 (991) 500-3168',
+        Address: '681 Test Address Full Lenght Bartlett Street, Macdona, Indiana, 606'
     },
     {
-        Account: 2,
-        Name: "Erin Le",
-        Email: "erinle@sealoud.com",
-        Phone: "+1 (903) 550-3586",
-        Address: "208 Aurelia Court, Muir, Maryland, 6116"
+        Account: 9822,
+        Name: 'Erin Le',
+        Email: 'erinle@sealoud.com',
+        Phone: '+1 (903) 550-3586',
+        Address: '208 Aurelia Court, Muir, Maryland, 6116'
     },
     {
-        Account: 3,
-        Name: "Ilene Cameron",
-        Email: "ilenecameron@sealoud.com",
-        Phone: "+1 (927) 433-3627",
-        Address: "469 Bath Avenue, Woodburn, Washington, 8982"
+        Account: 3952,
+        Name: 'Ilene Cameron',
+        Email: 'ilenecameron@sealoud.com',
+        Phone: '+1 (927) 433-3627',
+        Address: '469 Bath Avenue, Woodburn, Washington, 8982'
     },
     {
-        Account: 4,
-        Name: "Tamika Armstrong",
-        Email: "tamikaarmstrong@sealoud.com",
-        Phone: "+1 (859) 402-2190",
-        Address: "625 Landis Court, Teasdale, Idaho, 1342"
+        Account: 4745,
+        Name: 'Tamika Armstrong',
+        Email: 'tamikaarmstrong@sealoud.com',
+        Phone: '+1 (859) 402-2190',
+        Address: '625 Landis Court, Teasdale, Idaho, 1342'
     },
     {
-        Account: 5,
-        Name: "Silva Neal",
-        Email: "silvaneal@sealoud.com",
-        Phone: "+1 (838) 599-2644",
-        Address: "936 Madison Place, Newcastle, New Jersey, 1076"
+        Account: 5635,
+        Name: 'Silva Neal',
+        Email: 'silvaneal@sealoud.com',
+        Phone: '+1 (838) 599-2644',
+        Address: '936 Madison Place, Newcastle, New Jersey, 1076'
     },
     {
-        Account: 6,
-        Name: "Betsy Mcintyre",
-        Email: "betsymcintyre@sealoud.com",
-        Phone: "+1 (816) 427-2100",
-        Address: "583 Cozine Avenue, Rose, Nebraska, 5281"
+        Account: 6526,
+        Name: 'Betsy Mcintyre',
+        Email: 'betsymcintyre@sealoud.com',
+        Phone: '+1 (816) 427-2100',
+        Address: '583 Cozine Avenue, Rose, Nebraska, 5281'
+    },
+    {
+        Account: 4726,
+        Name: 'Betsy Mcintyre',
+        Email: 'betsymcintyre@sealoud.com',
+        Phone: '+1 (816) 427-2100',
+        Address: '583 Cozine Avenue, Rose, Nebraska, 5281'
+    },
+    {
+        Account: 6676,
+        Name: 'Betsy Mcintyre',
+        Email: 'betsymcintyre@sealoud.com',
+        Phone: '+1 (816) 427-2100',
+        Address: '583 Cozine Avenue, Rose, Nebraska, 5281'
+    },
+    {
+        Account: 1426,
+        Name: 'Betsy Mcintyre',
+        Email: 'betsymcintyre@sealoud.com',
+        Phone: '+1 (816) 427-2100',
+        Address: '583 Cozine Avenue, Rose, Nebraska, 5281'
     }
 ];
+
 @Component({
     selector: 'app-client',
     templateUrl: './client.component.html',
     styleUrls: ['./client.component.scss']
 })
 export class ClientComponent implements OnInit {
-    displayedColumns: any[] = ['Account', 'Name', 'Email', 'Phone', 'Address', 'btn'];
+    displayedColumns: any[] = ['Index', 'Account', 'Name', 'Email', 'Phone', 'Address', 'btn'];
     dataSource = ELEMENT_DATA;
 
-    foods: any[] = [
-        {value: 'steak-0', viewValue: 'All'},
-        {value: 'steak-1', viewValue: 'Steak'},
-        {value: 'pizza-2', viewValue: 'Pizza'},
-        {value: 'tacos-3', viewValue: 'Tacos'}
+    advocates: any[] = [
+        {value: '1', viewValue: 'All'},
+        {value: '2', viewValue: 'Steak'},
+        {value: '3', viewValue: 'Pizza'},
+        {value: '4', viewValue: 'Tacos'}
     ];
-  constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+    constructor(private router: Router) {
+    }
+
+    ngOnInit() {
+    }
 
     goToClientIntake() {
         this.router.navigate(['clientIntake']);
--- src/app/client/new-intake/new-intake.component.ts
+++ src/app/client/new-intake/new-intake.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-new-intake',
-  templateUrl: './new-intake.component.html',
-  styleUrls: ['./new-intake.component.scss']
+    selector: 'app-new-intake',
+    templateUrl: './new-intake.component.html',
+    styleUrls: ['./new-intake.component.scss']
 })
 export class NewIntakeComponent implements OnInit {
+    form: FormGroup;
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder) {
+        this.form = this.formBuilder.group({
+            firstName: [null, [Validators.required]],
+            middleName: [null],
+            lastName: [null, [Validators.required]],
+            date: [new Date(), [Validators.required]],
+            dateOfBirth: [new Date(), [Validators.required]],
+            email: [null, [Validators.required]]
+        });
+        // this.firstName = this.form.controls['firstName'];
+        // this.middleName = this.form.controls['middleName'];
+        // this.lastName = this.form.controls['lastName'];
+    }
+
     date3: Date;
     states = [
         {name: 'Arizona', abbrev: 'AZ'},
@@ -21,7 +36,17 @@ export class NewIntakeComponent implements OnInit {
         {value: 'pizza-1', viewValue: 'Pizza'},
         {value: 'tacos-2', viewValue: 'Tacos'}
     ];
-  ngOnInit() {
-  }
+
+    ngOnInit() {
+    }
+
+    private saveData(event) {
+        event.preventDefault();
+
+        if (this.form.valid) {
+            console.log(this.form.value); // Process your form
+        }
+        // console.log(this.form.firstName.value);
+    }
 
 }
